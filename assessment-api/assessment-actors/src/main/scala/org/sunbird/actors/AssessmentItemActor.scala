29ffe0be3c048c277dd66d313688b1e896718061
Rakshitha-D <rakshithagowda.d@gmail.com>
2025-11-17 12:01:06 +0530

    updated assessmentactor to be backward compatible to support node_id

--- assessment-api/assessment-actors/src/main/scala/org/sunbird/actors/AssessmentItemActor.scala
+++ assessment-api/assessment-actors/src/main/scala/org/sunbird/actors/AssessmentItemActor.scala
@@ -42,7 +42,7 @@ class AssessmentItemActor @Inject()(implicit oec: OntologyEngineContext) extends
     AssessmentItemUtils.flattenMetadataToRequest(request, metadata)
     if (!skipValidation) AssessmentItemValidator.validateAssessmentItemRequest(requestData, "ASSESSMENT_ITEM_CREATE")
     DataNode.create(request).map { node =>
-      ResponseHandler.OK.putAll(Map("identifier" -> node.getIdentifier.replace(".img", ""), "versionKey" -> node.getMetadata.get("versionKey")).asJava)
+      ResponseHandler.OK.putAll(Map("identifier" -> node.getIdentifier.replace(".img", ""), "node_id" -> node.getIdentifier.replace(".img", ""), "versionKey" -> node.getMetadata.get("versionKey")).asJava)
     }
   }
 
